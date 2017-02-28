950f33a93552ddd5111e5eae01bb1522daddd8e7
Erik Lilja <erikvlilja@gmail.com>
2017-02-28 09:07:07 +0100

    Add test resource, fix jar FileNotFoundEx

--- get_latest_mcc_table.bash
+++ get_latest_mcc_table.bash
@@ -8,4 +8,6 @@ which wget > /dev/null || eexit "ERROR: wget not installed"
 if [ ! -f "mcc-mnc-table.json" ]
 then
   wget http://raw.githubusercontent.com/musalbas/mcc-mnc-table/master/mcc-mnc-table.json
+  cp mcc-mnc-table.json src/main/resources/
+  cp mcc-mnc-table.json src/test/resources/
 fi
