464a5f8a259fc427bdb38bfa41c5c59979528703
Johan Bjäreholt <johan@bjareho.lt>
2017-04-05 12:51:09 +0200

    Refactoring main file

--- src/main/scala/se/qvantel/generator/CDRGenerator.scala
+++ src/main/scala/se/qvantel/generator/CDRGenerator.scala
@@ -21,33 +21,34 @@ object CDRGenerator extends App with SparkConnection
   logger.info("Config: Nr of maximum batches: " + nrOfMaximumBatches)
   logger.info("Config: batch element size: " + maxBatchSize)
 
-  def getLastSync(): DateTime = {
+  def getStartTime(): DateTime = {
+    val backInTimeTs = DateTime.now(DateTimeZone.UTC).minusHours(backInTimeHours)
+
     val cdrRdd = context.cassandraTable(keyspace, cdrTable)
     // If sudden crash, look into the last inserted record and begin generating from that timestamp
     val rows = cdrRdd.select("created_at")
+      .where("created_at>0")
       .where("clustering_key=0")
-      .clusteringOrder(rdd.ClusteringOrder.Descending)
+      .clusteringOrder(rdd.ClusteringOrder.Ascending)
       .limit(1)
       .collect()
 
-    // If there is no records in the cassandra database, the time to start the generator is now.
-    if (rows.length < 1) {
-      new DateTime(0, DateTimeZone.UTC)
+    // By default set startTs to backInTimeTs
+    var startTs = backInTimeTs
+    // If events exists in cassandra and last event is newer than backInTimeTs, start at lastEventTs
+    // This is done in case for example the CDR Generator crashes or is shut down it will continue where it stopped
+    if (rows.length > 0) {
+      val lastEventTs = new DateTime(rows.apply(0).getLong(0)/1000, DateTimeZone.UTC)
+      logger.info(s"BackInTimeTs: $backInTimeTs")
+      logger.info(s"LastEventTs: $lastEventTs")
+      if (lastEventTs.getMillis > backInTimeTs.getMillis) {
+        startTs = lastEventTs
+      }
     }
-    else {
-      // Otherwise, get the latest record and generate from that timestamp to now.
-      val lastSyncNs = rows.apply(0).getLong(0)
-      new DateTime(lastSyncNs / 1000, DateTimeZone.UTC)
-    }
+    startTs
   }
 
-  val lastSync = getLastSync()
-  var startTs = DateTime.now(DateTimeZone.UTC).minusHours(backInTimeHours)
-  logger.info(s"Last sync ts: $lastSync")
-  logger.info(s"Back in time ts: $startTs")
-  if (lastSync.getMillis > startTs.getMillis){
-    startTs = lastSync
-  }
+  val startTs = getStartTime()
   logger.info(s"Start ts: $startTs")
 
   var products = Trends.readTrendsFromFile(startTs)
@@ -57,24 +58,23 @@ object CDRGenerator extends App with SparkConnection
 
     val nextEntry = products.head
     val product = nextEntry._1
-    val tsMillis = nextEntry._2
-    // Convert epoch timestamp from milli seconds to nano seconds
-    val tsNanos = tsMillis*1000 + (Random.nextInt()%1000)
-    val ts = new DateTime(tsMillis, DateTimeZone.UTC)
+    val ts = new DateTime(nextEntry._2, DateTimeZone.UTC)
 
+    // Sleep until next event to be generated
+    val sleeptime = ts.getMillis - DateTime.now(DateTimeZone.UTC).getMillis
+    if (sleeptime >= 0) {
+      Thread.sleep(sleeptime)
+    }
+
+    // Debug print
+    val productname = product.name
+    logger.info(s"$ts - $productname")
+
+    // Generate and send CDR
     val execBatch = Try {
-      // Sleep until next event to be generated
-      val sleeptime = tsMillis - DateTime.now(DateTimeZone.UTC).getMillis
-      logger.info(products.toString)
-      if (sleeptime >= 0) {
-        Thread.sleep(sleeptime)
-      }
-
-      // Debug print
-      val productname = product.name
-      logger.info(s"$ts - $productname")
-
-      // Generate CDR
+      // Convert epoch timestamp from milli seconds to micro seconds
+      val tsNanos = ts.getMillis*1000 + (System.nanoTime()%1000)
+      // Generate CQL query for EDR
       val edrQuery = EDR.generateRecord(product, tsNanos)
       batch.add(new SimpleStatement(edrQuery))
 
@@ -90,7 +90,7 @@ object CDRGenerator extends App with SparkConnection
     execBatch match {
       case Success(_) => {
         // Calculate next time this type of event should be generated
-        val nextTs = tsMillis + Trends.nextTrendEventSleep(product, ts)
+        val nextTs = ts.getMillis + Trends.nextTrendEventSleep(product, ts)
         products = products + (product -> nextTs)
       }
       case Failure(e) => {
