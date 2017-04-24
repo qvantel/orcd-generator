d441a6b01ffd09e0d1e613e5d986ac770c62b347
Johan Bjäreholt <johan@bjareho.lt>
2017-04-24 14:59:20 +0200

    Rewrote EDR generation

--- src/main/scala/se/qvantel/generator/model/EDR.scala
+++ src/main/scala/se/qvantel/generator/model/EDR.scala
@@ -3,85 +3,215 @@ package se.qvantel.generator.model
 import se.qvantel.generator.model.product.Product
 import se.qvantel.generator.GenerateData
 
+case class ServiceUnitModel
+(
+  amount: Int,
+  currency: String,
+  unitOfMeasure: String
+) {
+  def toCqlValues(): String = {
+    s"{amount: $amount, currency: '$currency', unit_of_measure: '$unitOfMeasure'}"
+  }
+}
+
+case class LocationInformationModel
+(
+  destination: String,
+  locationNumber: String,
+  locationAreaIdentification: String,
+  cellGlobalIdentification: String
+) {
+  def toCqlValues(): String = {
+    "{" +
+      s"destination: '$destination', " +
+      s"location_number: '$locationNumber', " +
+      s"location_area_identification: '$locationAreaIdentification', " +
+      s"cell_global_identification: '$cellGlobalIdentification'"+
+    "}"
+  }
+}
+
+case class EventDetailsModel
+(
+  accessPointName: String,
+  trafficCase: String,
+  eventType: String,
+  isRoaming: Boolean,
+  aPartyNumber: String,
+  bPartyNumber: String,
+  aPartyLocation: LocationInformationModel,
+  bPartyLocation: LocationInformationModel
+) {
+  def toCqlValues(): String = {
+    val apl = aPartyLocation.toCqlValues()
+    var bpl = "null"
+    if (bPartyLocation != null){
+      bpl = bPartyLocation.toCqlValues()
+    }
+    s"{" +
+      s"access_point_name: '$accessPointName', " +
+      s"traffic_case: '$trafficCase', " +
+      s"event_type: '$eventType', " +
+      s"is_roaming: $isRoaming, " +
+      s"a_party_number: '$aPartyNumber', " +
+      s"b_party_number: '$bPartyNumber', " +
+      s"a_party_location: $apl, " +
+      s"b_party_location: $bpl" +
+    "}"
+  }
+}
+
+case class ChargedAmountsModel
+(
+  id: String,
+  name: String,
+  chargedType: String,
+  eventType: String,
+  resourceType: String,
+  amount: Int,
+  endBalance: Int,
+  expiryDate: String
+) {
+  def toCqlValues(): String = {
+    "{" +
+      s"id: '$id', " +
+      s"name: '$name', " +
+      s"charged_type: '$chargedType', " +
+      s"event_type: '$eventType', " +
+      s"resource_type: '$resourceType', " +
+      s"amount: $amount, " +
+      s"end_balance: $endBalance, " +
+      s"expiry_date: '$expiryDate'" +
+    "}"
+  }
+}
+
+case class ProductModel
+(
+  id: String,
+  name: String
+) {
+  def toCqlValues(): String = {
+    "{" +
+      s"id: '$id', " +
+      s"name: '$name'" +
+    "}"
+  }
+}
+
+case class EventChargesModel
+(
+  chargedUnits: ServiceUnitModel,
+  product: ProductModel,
+  chargedAmounts: ChargedAmountsModel
+)
+{
+  def toCqlValues(): String = {
+    val cu = chargedUnits.toCqlValues()
+    val p = product.toCqlValues()
+    val ca = chargedAmounts.toCqlValues()
+    s"{" +
+      s"charged_units: $cu, " +
+      s"product: $p, " +
+      s"charged_amounts: {$ca}" +
+    "}"
+  }
+}
+
+case class EDRModel
+(
+  createdAt: Long,
+  startedAt: Long,
+  service: String,
+  usedServiceUnits: ServiceUnitModel,
+  eventDetails: EventDetailsModel,
+  eventCharges: EventChargesModel,
+  clusteringKey: Int
+) {
+  def toCqlValues(): String = {
+    val usu = usedServiceUnits.toCqlValues()
+    val ed = eventDetails.toCqlValues()
+    val ec = eventCharges.toCqlValues()
+    s"uuid(), $createdAt, $startedAt, '$service', $usu, $ed, $ec, $clusteringKey"
+  }
+}
+
 object EDR {
-  def amount: Int = GenerateData.amount()
-  def unitOfMeasure: String = GenerateData.unitOfMeasure()
-  def isRoaming: Boolean = GenerateData.isRoaming()
-  def aPartyNumber: String = GenerateData.msisdn()
-  def apnDestination: String = GenerateData.destination()
-  val clustering_key = 0
-  var timestamp : Long = 0
-  val apn_location_number = ""
-  val apn_location_area_identification = ""
-  val apn_cell_global_identification = ""
-  val currency = ""
-  val productId = ""
-  var productName = ""
-  val caid = ""
-  val caname = ""
-  var service  = ""
-  val cactype = ""
-  val caetype = ""
-  val cartype = ""
-  def endBalance: Int = GenerateData.amount()
-  val expiry_date = ""
+  private def generateLocationInformation(): LocationInformationModel ={
+    LocationInformationModel(
+      destination = GenerateData.destination(),
+      locationNumber = "",
+      locationAreaIdentification = "",
+      cellGlobalIdentification = ""
+    )
+  }
 
   def generateRecord(product: Product, tsNanos: Long): String = {
-    service = product.serviceType
-    productName = product.name
-    timestamp = tsNanos
-    service match {
-      case "voice" => generateVoiceRecord()
-      case _ => generateDataRecord()
+    var eventDetails = EventDetailsModel(
+      accessPointName = GenerateData.destination(),
+      trafficCase = "",
+      eventType = "",
+      isRoaming = GenerateData.isRoaming(),
+      aPartyNumber = GenerateData.msisdn(),
+      bPartyNumber = null,
+      aPartyLocation = generateLocationInformation(),
+      bPartyLocation = null
+    )
+    if (product.serviceType == "voice"){
+      eventDetails = eventDetails.copy(
+        eventDetails.accessPointName,
+        eventDetails.trafficCase,
+        eventDetails.eventType,
+        eventDetails.isRoaming,
+        eventDetails.aPartyNumber,
+        eventDetails.bPartyNumber,
+        eventDetails.aPartyLocation,
+        generateLocationInformation()
+      )
     }
+    val usedServiceUnits = ServiceUnitModel(
+      amount = GenerateData.amount(),
+      currency = "",
+      unitOfMeasure = GenerateData.unitOfMeasure()
+    )
+    val chargedServiceUnits = ServiceUnitModel(
+      amount = GenerateData.amount(),
+      currency = "",
+      unitOfMeasure = GenerateData.unitOfMeasure()
+    )
+    val chargedAmounts = ChargedAmountsModel(
+      id = "",
+      name = "",
+      chargedType = "",
+      eventType = "",
+      resourceType = "",
+      amount = usedServiceUnits.amount,
+      endBalance = GenerateData.amount(),
+      expiryDate = ""
+    )
+    val prod = ProductModel(
+      id = product.campaignId,
+      name = product.name
+    )
+    val eventCharges = EventChargesModel(
+      chargedUnits = chargedServiceUnits,
+      product = prod,
+      chargedAmounts = chargedAmounts
+    )
+    val edr = EDRModel(
+      createdAt = tsNanos,
+      startedAt = tsNanos,
+      service = product.serviceType,
+      usedServiceUnits = usedServiceUnits,
+      eventDetails = eventDetails,
+      eventCharges = eventCharges,
+      clusteringKey = 0
+    )
+    generateRecord(edr)
   }
 
-  private def generateVoiceRecord(): String = {
-    // Call specific generation variables
-    val trafficCase = GenerateData.trafficCase()
-    val bPartyNumber = GenerateData.msisdn()
-    val bpnDestination = GenerateData.destination()
-    val bpn_location_number = ""
-    val bpn_location_area_identification = ""
-    val bpn_cell_global_identification = ""
-
-    s"INSERT INTO qvantel.cdr (id, created_at, started_at, clustering_key, used_service_units, service, event_details, event_charges)" +
-    s"VALUES (uuid(), $timestamp, $timestamp, $clustering_key, " + // id, created_at, started_at, clustering_key
-    s"{amount:$amount, unit_of_measure:'$unitOfMeasure', currency: '$currency'}, " + // used_service_units
-    s"'$service'," + // service
-    s"{traffic_case: '$trafficCase', event_type: '$service', a_party_number: '$aPartyNumber', " + //event_details
-    s"b_party_number: '$bPartyNumber', is_roaming: $isRoaming, a_party_location: {" + // event_details
-    s"destination: '$apnDestination', location_number: '$apn_location_number', " + //a_party_location
-    s"location_area_identification: '$apn_location_area_identification', " +
-    s"cell_global_identification: '$apn_cell_global_identification'}, " +
-    s"b_party_location: {" + // b_party_location
-    s"destination: '$bpnDestination', location_number: '$bpn_location_number', " +
-    s"location_area_identification: '$bpn_location_area_identification', " +
-    s"cell_global_identification: '$bpn_cell_global_identification'}}, " +
-    s"{charged_units: {amount: $amount, unit_of_measure: '$unitOfMeasure', currency: '$currency'}," + //charged units
-    s"product: {id: '$productId', name: '$productName'}," + //product
-    s"charged_amounts: {{id: '$caid', name: '$caname', charged_type: '$cactype', event_type: '$caetype'," + //charged amounts
-    s"resource_type: '$cartype', amount: $amount, end_balance: $endBalance, expiry_date: '$expiry_date'}}" +
-    s"});" // end of cassandra statement
-  }
-
-  private def generateDataRecord(): String = {
-    // Product specific generation variables
-    val apname = ""
-
-    s"INSERT INTO qvantel.cdr (id, created_at, started_at, clustering_key, used_service_units, service, event_details, event_charges)" +
-    s"VALUES (uuid(), $timestamp, $timestamp, $clustering_key, " + // id, created_at, started_at, clustering_key
-    s"{amount:$amount, unit_of_measure:'$unitOfMeasure', currency: '$currency'}, " + // used_service_units
-    s"'$service'," + // service
-    s"{access_point_name: '$apname', a_party_number: '$aPartyNumber', " + //event_details
-    s"is_roaming: $isRoaming, a_party_location: {" + // event_details
-    s"destination: '$apnDestination', location_number: '$apn_location_number', " + //a_party_location
-    s"location_area_identification: '$apn_location_area_identification', " +
-    s"cell_global_identification: '$apn_cell_global_identification'}}, " +
-    s"{charged_units: {amount: $amount, unit_of_measure: '$unitOfMeasure', currency: '$currency'}," + //charged units
-    s"product: {id: '$productId', name: '$productName'}," + //product
-    s"charged_amounts: {{id: '$caid', name: '$caname', charged_type: '$cactype', event_type: '$caetype'," + //charged amounts
-    s"resource_type: '$cartype', amount: $amount, end_balance: $endBalance, expiry_date: '$expiry_date'}}" +
-    s"});" // end of cassandra statement
+  private def generateRecord(edr: EDRModel): String = {
+    "INSERT INTO qvantel.cdr (id, created_at, started_at, service, used_service_units, event_details, event_charges, clustering_key) " +
+    "VALUES (" + edr.toCqlValues() + ");"
   }
 }
